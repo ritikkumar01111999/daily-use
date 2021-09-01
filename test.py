162ee30d8c061922de14dc846624f5e537d8b718
RITIK KUMAR <89376306+ritikkumar01111999@users.noreply.github.com>
2021-09-01 22:16:18 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,79 @@
+from flask import Flask,request,jsonify
+app=Flask(__name__)
+@app.route("/api/add",method=['GET'])
+def add():
+    """This is a function to recive values from requestobject
+    complete the calculation 
+    format the result into jason 
+    return the json response object""""
+
+
+    number1=request.args.get("enter the first number",type=int)
+    number2=request.args.get("enter the second number",type=int)
+    total=number1+number2
+    response=make_response(
+        jsonify({"result":str(total)}),
+
+    
+
+    )
+    response.headers["content-Type"]="application/json"
+    #return response
+    return
+@app.route("/api/sub",method=['GET'])
+def add():
+    """This is a function to recive values from requestobject
+    complete the calculation 
+    format the result into jason 
+    return the json response object""""
+
+    number1=request.args.get("enter the first number",type=int)
+    number2=request.args.get("enter the second number",type=int)
+    total=number1-number2
+    response=make_response(
+        jsonify({"result":str(total)}),
+
+    
+
+    )
+    response.headers["content-Type"]="application/json"
+    #return response
+    return
+@app.route("/api/sMUL",method=['GET'])
+def add():
+    """This is a function to recive values from requestobject
+    complete the calculation 
+    format the result into jason 
+    return the json response object""""
+
+    number1=request.args.get("enter the first number",type=int)
+    number2=request.args.get("enter the second number",type=int)
+    total=number1*number2
+    response=make_response(
+        jsonify({"result":str(total)}),
+
+    
+
+    )
+    response.headers["content-Type"]="application/json"
+    #return response
+    return
+@app.route("/api/DIV",method=['GET'])
+def add():
+    """This is a function to recive values from requestobject
+    complete the calculation 
+    format the result into jason 
+    return the json response object""""
+
+    number1=request.args.get("enter the first number",type=int)
+    number2=request.args.get("enter the second number",type=int)
+    total=number1/number2
+    response=make_response(
+        jsonify({"result":str(total)}),
+
+    
+
+    )
+    response.headers["content-Type"]="application/json"
+    #return response
+    return
