35bf43a04dcabc6182222fa42cf7e622c2aee859
RITIK KUMAR <89376306+ritikkumar01111999@users.noreply.github.com>
2021-09-02 16:22:08 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,7 +1,8 @@
+#program for the checking the value is plindrome or not
 def cal(i):
     j=i[::-1]
     
-
+#condiiton to check for the palindrome
     if i==j:
         print('the value of the string is palindrome ')
     else:
